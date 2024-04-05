578c198cea34eaaad1d487c7c1de0cb976f1c860
pstemporowski <stemper0123@gmail.com>
2024-04-05 20:40:33 +0200

    more filtering algorithms

--- extract_data.py
+++ extract_data.py
@@ -8,6 +8,7 @@ from urllib.parse import urlparse
 from utils.logger import logger
 import time
 import locale
+import re
 
 locale.setlocale(locale.LC_TIME, "de_DE")
 
@@ -15,7 +16,7 @@ dotenv.load_dotenv()
 GPT_MODEL = os.environ["GPT_MODEL"]
 GPT_API_KEY = os.environ["GPT_API_KEY"]
 GPT_MAX_TOKENS = int(os.environ["GPT_MAX_TOKENS"])
-CITY = "munchen"
+CITY = "frankfurt"
 DATA_DIR = f"./data/{CITY}/"
 PARTIES_COLS = ["title", "genre", "club_name", "time"]
 URLS_COLS = ["url"]
@@ -31,7 +32,7 @@ def main():
     parties_path = os.path.join(DATA_DIR, "parties.csv")
     temp_path = os.path.join(DATA_DIR, "temp_1.csv")
     urls_path = os.path.join(DATA_DIR, "urls.csv")
-    urls = pd.read_csv(urls_path)
+    urls = pd.read_csv(urls_path, sep=";", na_values=["None"])
     parties_df = None
 
     if os.path.exists(parties_path):
@@ -39,39 +40,73 @@ def main():
     else:
         parties_df = pd.DataFrame(columns=PARTIES_COLS)
 
-    for i, (url, mode, split_cls, club_name, genre) in urls.iterrows():
+    for i, (
+        url,
+        mode,
+        split_cls,
+        filter_regex,
+        club_name,
+        genre,
+        filter_title,
+        link_cls,
+    ) in urls.iterrows():
         df = get_parties(
             url,
             mode,
             split_cls,
+            filter_regex=filter_regex,
             dft_club_name=club_name,
             dft_genre=genre,
+            link_cls=link_cls,
         )
         parties_df = pd.concat([parties_df, df])
 
         if i % 5 == 0 and i != 0:
             parties_df.to_csv(temp_path, index=False)
 
-    parties_df = parties_df.drop_duplicates(keep="first")
+    parties_df = post_process_parties(parties_df, filter_title=filter_title)
     parties_df.to_csv(parties_path, index=False)
 
     if WRITE_PARTIES_PER_WEEK:
         write_parties_per_week(parties_df)
 
 
+def post_process_parties(parties_df: pd.DataFrame, filter_title=None):
+
+    if not pd.isnull(filter_title):
+        parties_df = parties_df[
+            ~parties_df["title"].str.contains(filter_title, case=False, regex=True)
+        ]
+
+    parties_df["club_name"] = parties_df["club_name"].str.title()
+    parties_df["title"] = parties_df["title"].str.title()
+    parties_df = parties_df.drop_duplicates(keep="first", ignore_index=True)
+    parties_df = parties_df.sort_values(by="time")
+    parties_df = parties_df.reset_index(drop=True)
+    return parties_df
+
+
 def get_parties(
-    url: str, mode="default", split_cls_name=None, dft_club_name=None, dft_genre=None
+    url: str,
+    mode="default",
+    split_cls_name=None,
+    filter_regex=None,
+    dft_club_name=None,
+    dft_genre=None,
+    link_cls=None,
 ):
     # Fetch and parse the HTML content from the URL of the events page
     html = get_html(url)
 
     if mode == "default" or mode == "detail" or mode == "links":
-        links = get_links_from_overview(html, url)
+        links = get_links_from_overview(html, url, link_cls=link_cls)
 
         if len(links) == 0:
-            logger.warn(f"No links found to extract information from on {url}.")
+            logger.warning(f"No links found to extract information from on {url}.")
         else:
-            logger.info(f"Found {len(links)} links to extract information from.")
+            logger.info(
+                f"Found {len(links)} links to extract information from on {url}."
+            )
 
         return get_parties_from_detail_pages(links)
     elif mode == "overview":
@@ -80,24 +115,36 @@ def get_parties(
             split_cls_name=split_cls_name,
             dft_club_name=dft_club_name,
             dft_genre=dft_genre,
+            filter_regex=filter_regex,
         )
     else:
         raise ValueError(f"Invalid mode: {mode}")
 
 
-def get_links_from_overview(html, url):
-    links_df = ext_links(html, return_pd=True)
+def get_links_from_overview(html, url, link_cls=None):
+    links_df = ext_links(html, return_pd=True, link_cls=link_cls)
     parsed_url = urlparse(url)
     base_url = f"{parsed_url.scheme}://{parsed_url.netloc}"
-    links_df = clean_links(
-        links_df, base_url=base_url, contains_list=["party", "event"]
+    contains_list = None
+
+    if pd.isnull(link_cls):
+        contains_list = ["party", "event", "veranstaltung", "programm", "program"]
+
+    links_df = post_process_links(
+        links_df,
+        base_url=base_url,
+        contains_list=contains_list,
     )
 
     return links_df["links"]
 
 
 def get_parties_from_overview(
-    html, split_cls_name=None, dft_club_name=None, dft_genre=None
+    html,
+    split_cls_name=None,
+    dft_club_name=None,
+    dft_genre=None,
+    filter_regex=None,
 ):
     assert split_cls_name is not None, "split_class_name must be provided."
 
@@ -105,16 +152,23 @@ def get_parties_from_overview(
     soup = BeautifulSoup(html, "html.parser")
     soup = soup.find_all(class_=split_cls_name)
     for i, s in enumerate(soup):
+
+        if filter_regex and filter_regex != "None":
+            pattern = re.compile(filter_regex)
+            if not pattern.search(str(s)):
+                logger.info(f"Skipping split because it does not match the filter.")
+                continue
+
         txt = s.get_text(strip=True)
 
         try:
             ctx = extr_info_with_llm(txt)
             ctx = ctx.split(";")
 
-            if dft_club_name:
+            if not pd.isnull(dft_club_name):
                 ctx[2] = dft_club_name
 
-            if dft_genre and ctx[1] == "None" or ctx[1] == "Mixed":
+            if not pd.isnull(dft_genre) and ctx[1] == "None" or ctx[1] == "Mixed":
                 ctx[1] = dft_genre
 
             if len(ctx) != 4:
@@ -169,7 +223,7 @@ def extr_info_with_llm(txt: str):
         Die beste 90s Hits;Techno;Batschkapp;2022-10-19 20:00:00;
     """
     # Here, you formulate a prompt for the LLM to instruct it on what information to extract.
-    prompt = f"""Extract the title, genre, club name, datetime from this description in german: "{txt}". Choose a genre out of the text between Techno, Electronic, Rock, Metal, Hip-Hop, 80s, 90s, Pop and Mixed! The genres are very general so as they is for example PopPunk you can classify it as Pop and so on. As in the text are mentioned more than one genre that cannot be classified to one, like in the text is Electronic and Hip-Hop, so you gonna write Mixed. Also don't write the city name in the title or the club name. Moreover if the title is a sentence or a persons name write it case sensitivity. Try to rewrite the title as much as possible in case sensitive. You need to classify the provided information, if it is actually a party or not. For example there can be Situations where the event is stand up comedy or a 'flohmarkt' and so on, if it is the case whole answer blank!  Don't rewrite the content but you can short it somehow so that the title is no longer then 25 chars and format the output as: 
+    prompt = f"""Extract the title, genre, club name, datetime from this party description in german: "{txt}". Choose a genre out of the text between Techno, Electronic, Rock, Metal, Hip-Hop, 80s, 90s, Pop and Mixed! The genres are very general so as they is for example PopPunk you can classify it as Pop and so on. As in the text are mentioned more than one genre that cannot be classified to one, like in the text is Electronic and Hip-Hop, so you gonna write Mixed. Also don't write the city name in the title or the club name. Moreover if the title is a sentence or a persons name write it case sensitivity. Try to rewrite the title as much as possible in case sensitive. You need to classify the provided information, if it is actually a party or not. For example there can be Situations where the event is stand up comedy or a 'flohmarkt' and so on, if it is the case whole answer blank!  Don't rewrite the content but you can short it somehow so that the title is no longer then 25 chars and format the output as: 
 
     "{{Title}};{{Genre}};{{Clubname}};{{Date in YYYY-MM-DD HH:MM:SS}}"
     
@@ -204,15 +258,38 @@ def extr_info_with_llm(txt: str):
 
 def get_html(url: str):
     # Fetch HTML content
-    response = requests.get(url, timeout=10)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+    }
+    response = requests.get(
+        url,
+        timeout=10,
+        headers=headers,
+    )
     html = response.text
+
     return html
 
 
-def get_links(html):
+def get_links(html, link_cls=None) -> list:
     # Get all links from the HTML content
+    links = []
+    a_tags = []
     soup = BeautifulSoup(html, "html.parser")
-    return soup.find_all("a")
+
+    if pd.isnull(link_cls):
+        a_tags = soup.find_all("a")
+    else:
+        elements = soup.find_all(class_=link_cls)
+        for el in elements:
+            a_tags.extend(el.find_all("a"))
+
+    for a in a_tags:
+        link = a.get("href")
+        if link:
+            links.append(link)
+
+    return links
 
 
 def parse_html_2_txt(html):
@@ -221,24 +298,16 @@ def parse_html_2_txt(html):
     return soup.get_text(strip=True)
 
 
-def ext_links(content, return_pd=False):
-    links = []
-    all_links = get_links(content)
-
+def ext_links(content, return_pd=False, link_cls=None):
+    links = get_links(content, link_cls=link_cls)
     # Get all the links that provide to other detail pages of parties and events
-    for link in all_links:
-        link = link.get("href")
-
-        if link:
-            links.append(link)
-
     if return_pd:
         return pd.DataFrame(links, columns=["links"])
 
     return links
 
 
-def clean_links(links_df, base_url=None, contains_list=None, rm_duplicates=True):
+def post_process_links(links_df, base_url=None, contains_list=None, rm_duplicates=True):
     if base_url:
         # Make all links absolute
         links_df["links"] = links_df["links"].apply(
@@ -264,11 +333,17 @@ def is_absolute(url):
 
 
 def write_parties_per_week(parties_df: pd.DataFrame):
+    parties_per_week_dir = os.path.join(DATA_DIR, "parties_per_week")
+    os.makedirs(parties_per_week_dir, exist_ok=True)
+
     parties_df["time"] = pd.to_datetime(
         parties_df["time"],
         errors="coerce",
         format="mixed",
     )
+    parties_df = parties_df[
+        parties_df["time"] <= pd.Timestamp.now() + pd.DateOffset(months=6)
+    ]
     parties_df = parties_df.sort_values(by="time")
     parties_df["week"] = parties_df["time"].dt.isocalendar().week
     weeks = parties_df["week"].unique()
@@ -276,22 +351,27 @@ def write_parties_per_week(parties_df: pd.DataFrame):
     for week in weeks:
         week_df = parties_df[parties_df["week"] == week]
         week_df = week_df.drop(columns=["week"])
+        week_df = week_df.sort_values(by="time")
         week_txt = ""
         for _, row in week_df.iterrows():
             title = row["title"]
             genre = row["genre"]
             club_name = row["club_name"]
             time = row["time"].strftime("%a. %d.%m")
-            hour = row["time"].strftime("%H:%M")
+            hour = (
+                row["time"].strftime("%H:%M")
+                if row["time"].minute != 0
+                else row["time"].strftime("%H")
+            )
             if genre:
                 party_info = f"{title} ({genre})"
             else:
                 party_info = title
             party_info += f"\n{club_name} - {time} ab {hour} Uhr\n\n"
             week_txt += party_info
-        with open(
-            os.path.join(DATA_DIR, f"parties_per_week/week_{week}.txt"), "w"
-        ) as file:
+
+        parties_per_week_path = os.path.join(parties_per_week_dir, f"week_{week}.txt")
+        with open(parties_per_week_path, "w") as file:
             file.write(week_txt)
 
 
