b6f83f6042fa26cabeb5ae1a4822adc12375b423
pstemporowski <stemper0123@gmail.com>
2024-04-23 21:00:23 +0200

    add better error handling  & update prompt & add manual entries & update .gitignore

--- extract_data.py
+++ extract_data.py
@@ -1,27 +1,45 @@
-import requests
-import dotenv
-import os
 from bs4 import BeautifulSoup
 from openai import OpenAI
-import pandas as pd
 from urllib.parse import urlparse
-from utils.logger import logger
-import time
+import dotenv
 import locale
+import os
+import pandas as pd
 import re
+import requests
+from requests.exceptions import ChunkedEncodingError, RequestException
+
+from utils.logger import logger
+from datetime import datetime
+from datetime import datetime
+import argparse
 
 locale.setlocale(locale.LC_TIME, "de_DE")
 
+parser = argparse.ArgumentParser(description="Script to extract data.")
+parser.add_argument("-city", type=str, help="City name", default="munchen")
+args = parser.parse_args()
+
 dotenv.load_dotenv()
-IMPORTANT_GENRES = ["Hip-Hop", "Rock", "Techno", "Metal"]
 GPT_MODEL = os.environ["GPT_MODEL"]
 GPT_API_KEY = os.environ["GPT_API_KEY"]
 GPT_MAX_TOKENS = int(os.environ["GPT_MAX_TOKENS"])
-CITY = "berlin"
+
+
+CITY = args.city
 DATA_DIR = f"./data/{CITY}/"
+IMPORTANT_GENRES = ["Hip-Hop", "Rock", "Techno", "Metal"]
 PARTIES_COLS = ["title", "genre", "club_name", "time"]
 URLS_COLS = ["url"]
 WRITE_PARTIES_PER_WEEK = True
+FILTER_TITLES = [
+    "flohmarkt",
+    "bundesliga",
+    "stand up comedy",
+    "comedy",
+    "kabarett",
+]
+
 
 gpt = OpenAI(
     api_key=GPT_API_KEY,  # this is also the default, it can be omitted
@@ -33,57 +51,80 @@ def main():
     parties_path = os.path.join(DATA_DIR, "parties.csv")
     temp_path = os.path.join(DATA_DIR, "temp_1.csv")
     urls_path = os.path.join(DATA_DIR, "urls.csv")
+    manual_entries_path = os.path.join(DATA_DIR, "manual_entries.csv")
     urls = pd.read_csv(urls_path, sep=";", na_values=["None"])
-    parties_df = None
+    parties_df = pd.DataFrame(columns=PARTIES_COLS)
+    manual_entries_df = pd.read_csv(manual_entries_path, header=0)
 
-    if os.path.exists(parties_path):
-        parties_df = pd.read_csv(parties_path, header=0)
-    else:
-        parties_df = pd.DataFrame(columns=PARTIES_COLS)
-
-    for i, (
-        url,
-        mode,
-        split_cls,
-        filter_regex,
-        club_name,
-        genre,
-        filter_title,
-        link_cls,
-    ) in urls.iterrows():
+    for i, row in urls.iterrows():
         df = get_parties(
-            url,
-            mode,
-            split_cls,
-            filter_regex=filter_regex,
-            dft_club_name=club_name,
-            dft_genre=genre,
-            link_cls=link_cls,
+            url=row["url"],
+            mode=row["mode"],
+            split_cls_name=row["split_by_class"],
+            filter_regex=row["filter_regex"],
+            dft_club_name=row["club_name"],
+            dft_genre=row["genre"],
+            link_cls=row["link_class"],
         )
         parties_df = pd.concat([parties_df, df])
 
         if i % 5 == 0 and i != 0:
             parties_df.to_csv(temp_path, index=False)
 
-    parties_df = post_process_parties(parties_df, filter_title=filter_title)
+    manual_entries_df = pre_process_manual_entries(manual_entries_df)
+    parties_df = pd.concat([parties_df, manual_entries_df])
+
+    parties_df = post_process_parties(parties_df, filter_titles=FILTER_TITLES)
     parties_df.to_csv(parties_path, index=False)
 
     if WRITE_PARTIES_PER_WEEK:
         write_parties_per_week(parties_df)
 
 
-def post_process_parties(parties_df: pd.DataFrame, filter_title=None):
+def pre_process_manual_entries(manual_entries_df: pd.DataFrame):
+    entries = []
+    for _, row in manual_entries_df.iterrows():
+        start_date = datetime.now()
+        end_date = start_date + pd.DateOffset(months=2)
+        delta = pd.DateOffset(days=7)
 
-    if not pd.isnull(filter_title):
-        parties_df = parties_df[
-            ~parties_df["title"].str.contains(filter_title, case=False, regex=True)
-        ]
+        new_entries = []
+        if pd.isnull(row["repeat"]):
+            new_entry = row.copy()
+            new_entry.drop("repeat", inplace=True)
+            new_entries.append(new_entry)
+        else:
+            for date in pd.date_range(start_date, end_date, freq=delta):
+                new_entry = row.copy()
+                new_entry["time"] = date
+                new_entry.drop("repeat", inplace=True)
+                new_entries.append(new_entry)
 
+        entries.extend(new_entries)
+    print(entries)
+    return pd.DataFrame(entries, columns=PARTIES_COLS)
+
+
+def post_process_parties(parties_df: pd.DataFrame, filter_titles=None):
+    if filter_titles:
+        flrt_re = "|".join(filter_titles)
+        fltr_mask = ~parties_df["title"].str.contains(
+            flrt_re, case=False, na=False, regex=True
+        )
+        parties_df = parties_df[fltr_mask]
+
+    parties_df = parties_df.dropna(subset=["club_name", "title"])
     parties_df["club_name"] = parties_df["club_name"].str.title()
     parties_df["title"] = parties_df["title"].str.title()
-    parties_df = parties_df.drop_duplicates(keep="first", ignore_index=True)
+    parties_df["time"] = pd.to_datetime(
+        parties_df["time"],
+        format="mixed",
+        errors="coerce",
+    )
+    parties_df = parties_df.drop_duplicates(
+        subset=["club_name", "title", "time"], keep="first"
+    ).reset_index(drop=True)
     parties_df = parties_df.sort_values(by="time")
-    parties_df = parties_df.reset_index(drop=True)
     return parties_df
 
 
@@ -98,6 +139,8 @@ def get_parties(
 ):
     # Fetch and parse the HTML content from the URL of the events page
     html = get_html(url)
+    if html is None:
+        return pd.DataFrame(columns=PARTIES_COLS)
 
     if mode == "default" or mode == "detail" or mode == "links":
         links = get_links_from_overview(html, url, link_cls=link_cls)
@@ -166,7 +209,7 @@ def get_parties_from_overview(
             ctx = extr_info_with_llm(txt)
             ctx = ctx.split(";")
 
-            if len(ctx) != 4:
+            if len(ctx) != 4 or pd.isnull(ctx[0]):
                 logger.warning(
                     f"Something went wrong with the extraction of the information for split {i}."
                 )
@@ -192,12 +235,15 @@ def get_parties_from_detail_pages(links, dft_club_name=None, dft_genre=None):
     for link in links:
         try:
             html = get_html(link)
+            if html is None:
+                continue
+
             txt = parse_html_2_txt(html)
 
             ctx = extr_info_with_llm(txt)
             ctx = ctx.split(";")
 
-            if len(ctx) != 4:
+            if len(ctx) != 4 or pd.isnull(ctx[0]):
                 logger.warning(
                     f"Something went wrong with the extraction of the information for the link: {link}"
                 )
@@ -230,19 +276,17 @@ def extr_info_with_llm(txt: str):
         Die beste 90s Hits;Techno;Batschkapp;2022-10-19 20:00:00;
     """
     # Here, you formulate a prompt for the LLM to instruct it on what information to extract.
-    prompt = f"""Extract the title, genre, club name, datetime from this party description in german: "{txt}". Choose a genre out of the text between Techno, Electronic, Rock, Metal, Hip-Hop, 80s, 90s, Pop and Mixed! The genres are very general so as they is for example PopPunk you can classify it as Pop and so on. As in the text are mentioned more than one genre that cannot be classified to one, like in the text is Electronic and Hip-Hop, so you gonna write Mixed. Also don't write the city name in the title or the club name. Moreover if the title is a sentence or a persons name write it case sensitivity. Try to rewrite the title as much as possible in case sensitive. You need to classify the provided information, if it is actually a party or not. For example there can be Situations where the event is stand up comedy or a 'flohmarkt' and so on, if it is the case whole answer blank!  Don't rewrite the content but you can short it somehow so that the title is no longer then 25 chars and format the output as: 
+    prompt = f"""Extract the title, genre, club name, and datetime from this German party description: "{txt}". Identify a genre from the options: Techno, Electronic, Rock, Metal, Hip-Hop, 80s, 90s, Pop, and Mixed. If the description includes a genre such as 'PopPunk', classify it under the nearest category, e.g., Pop. If multiple genres are mentioned that don’t align under one category, label it as Mixed. Base your genre choice on the event title and description; if uncertain, leave the genre field blank. Avoid including the city name in the title or club name. Format the title to ensure case sensitivity and shorten it if necessary to no more than 25 characters. Evaluate whether the text describes a party or another type of event like a 'flohmarkt' (flea market) or stand-up comedy, in which case, return a blank response. Output the results in the following format:
 
-    "{{Title}};{{Genre}};{{Clubname}};{{Date in YYYY-MM-DD HH:MM:SS}}"
-    
-    Example:
+        "{{Title}};{{Genre}};{{Clubname}};{{Date in YYYY-MM-DD HH:MM:SS}}"
+
+    Examples:
+    - Normal party:
     Die beste 90s Hits;Techno;Batschkapp;2022-10-19 20:00:00
-
-    Example for a blank answer where the party is flohmarkt:
+    - Event not a party (e.g., flea market):
     ;;;
-    
-    Example for a blank answer where the party is stand up comedy:
+    - Non-party event (e.g., stand-up comedy):
     ;;;
-    
     """
 
     # Call the OpenAI API to get the answer to your question
@@ -257,25 +301,34 @@ def extr_info_with_llm(txt: str):
         max_tokens=GPT_MAX_TOKENS,
         n=1,
         stop=None,
-        temperature=0.0,
+        temperature=0.15,
     )
 
     return res.choices[0].message.content
 
 
-def get_html(url: str):
+def get_html(url: str) -> str | None:
     # Fetch HTML content
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
     }
-    response = requests.get(
-        url,
-        timeout=10,
-        headers=headers,
-    )
-    html = response.text
 
-    return html
+    for _ in range(5):
+        try:
+
+            response = requests.get(
+                url,
+                timeout=10,
+                headers=headers,
+            )
+            return response.text
+        except RequestException:
+            continue
+        except ChunkedEncodingError:
+            continue
+
+    logger.error(f"Failed to fetch HTML content from {url}.")
+    return None
 
 
 def get_links(html, link_cls=None) -> list:
@@ -362,11 +415,6 @@ def write_parties_per_week(parties_df: pd.DataFrame):
     parties_per_week_dir = os.path.join(DATA_DIR, "parties_per_week")
     os.makedirs(parties_per_week_dir, exist_ok=True)
 
-    parties_df["time"] = pd.to_datetime(
-        parties_df["time"],
-        errors="coerce",
-        format="mixed",
-    )
     parties_df = parties_df[
         parties_df["time"] <= pd.Timestamp.now() + pd.DateOffset(months=6)
     ]
